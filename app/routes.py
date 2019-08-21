7a99718285e4e00a396f11765e9f4bd0c04072fd
Joe Holt <wynatoej@gmail.com>
2019-08-21 13:08:51 -0500

    added support for highlighting funnier caption

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ def index():
     with app.open_resource('data.json') as data_file:
         data = json.loads(data_file.read())
     data = data[0]
-    return render_template('index.html', url=data['img'], options=data['captions'], id=0)
+    return render_template('index.html', url=data['img'], options=data['captions'], id=0, winner=-1)
 
 @app.route('/next_cartoon', methods=["POST"])
 def next_cartoot():
@@ -20,7 +20,8 @@ def next_cartoot():
 
     if int(next_id) >= len(data):
         next_id = 0
+
     data = data[next_id]
-    return render_template('index.html', url=data['img'], options=data['captions'], id=next_id)
+    return render_template('index.html', url=data['img'], options=data['captions'], id=next_id, winner=-1)
 
 
