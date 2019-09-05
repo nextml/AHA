a6298c9c7e6963c6b65d84895def699b5e416539
Joe Holt <wynatoej@gmail.com>
2019-09-04 20:33:50 -0500

    Completed all work besides backend logic

--- app/routes.py
+++ app/routes.py
@@ -15,8 +15,9 @@ def index():
     data = load_data()[0]
     return render_template('index.html', url=data['img'], options=data['captions'], id=0, winner=-1, confidence=None, similar=None)
 
+
 @app.route('/next_cartoon', methods=["POST"])
-def next_cartoot():
+def next_cartoon():
 
     next_id = int(request.form['id']) + 1
     data = load_data()
@@ -28,3 +29,17 @@ def next_cartoot():
     return render_template('index.html', url=data['img'], options=data['captions'], id=next_id, winner=1, confidence=0.87, similar=["1", "2", "3"])
 
 
+@app.route('/compare_captions', methods=["POST"])
+def compare_captions():
+
+    user_caption = request.form['user_caption']
+    selected_caption = request.form['selected_caption']
+    funnier = 0
+    confidence = 0.90
+
+    id = int(request.form['id'])
+    data = load_data()[id]
+
+    return render_template('index.html', url=data['img'], options=data['captions'], id=id, winner=funnier, confidence=0.90, similar=['c1', 'c2', 'c3'])
+
+    return ''
